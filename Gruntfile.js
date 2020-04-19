1d742652bd65b1cfc29615506e4c24bc91b62531
Simon DELAGE <sdelage@gmail.com>
2020-04-19 21:38:40 +0200

    Added GRUNT and empty LESS files

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+
+        // Reference package.json
+        pkg: grunt.file.readJSON('package.json'),
+
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({
+                        overrideBrowserlist: ['last 2 versions']
+                    }),
+                    require('postcss-rtl')()
+                ]
+            },
+            dist: {
+                files: {
+                    'css/colormag.less': ['css/src/colormag.less'],
+                    'css/colormag.plugins.less': ['css/src/colormag.plugins.less'],
+                    'css/colormag.print.css': ['css/src/colormag.print.css'],
+                    'css/colormag.theme.less': ['css/src/colormag.theme.less']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/src/*.*',
+                tasks: ['postcss'],
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+};
