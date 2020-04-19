d05cb86edd1b5dec2c7683eeb5a001364fde13f2
Simon DELAGE <sdelage@gmail.com>
2020-04-19 23:15:15 +0200

    Footer copyright & buttons

--- tpl_footer.php
+++ tpl_footer.php
@@ -12,22 +12,29 @@ if (!defined('DOKU_INC')) die();
     <div class="pad">
         <?php tpl_license(''); // license text ?>
 
+
+        <div class="copyright">
+            Copyright &copy; 2020 <a href="https://demo.themegrill.com/colormag/" title="ColorMag" ><span>ColorMag</span></a> All rights reserved.
+            A <a href="https://wordpress.org/" title="WordPress"><span>WordPress</span></a> theme by <a href="https://themegrill.com/" title="ThemeGrill" rel="author"><span>ThemeGrill</span></a> ported to DokuWiki.
+        </div><!-- /.copyright -->
+
         <div class="buttons">
             <?php
                 tpl_license('button', true, false, false); // license button, no wrapper
                 $target = ($conf['target']['extern']) ? 'target="'.$conf['target']['extern'].'"' : '';
             ?>
+            <a href="https://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
+                src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15"
+                alt="Driven by DokuWiki" /></a>
+            <a href="https://translate.dokuwiki.org/" title="Localized (you can help)"><img src="<?php print tpl_basedir(); ?>images/button-localized.png" width="80" height="15" alt="Localized" /></a>
             <a href="https://www.dokuwiki.org/donate" title="Donate" <?php echo $target?>><img
-                src="<?php echo tpl_basedir(); ?>images/button-donate.gif" width="80" height="15" alt="Donate" /></a>
+                src="<?php echo tpl_basedir(); ?>images/button-donate.png" width="80" height="15" alt="Donate" /></a>
             <a href="https://php.net" title="Powered by PHP" <?php echo $target?>><img
-                src="<?php echo tpl_basedir(); ?>images/button-php.gif" width="80" height="15" alt="Powered by PHP" /></a>
+                src="<?php echo tpl_basedir(); ?>images/button-php.png" width="80" height="15" alt="Powered by PHP" /></a>
             <a href="//validator.w3.org/check/referer" title="Valid HTML5" <?php echo $target?>><img
                 src="<?php echo tpl_basedir(); ?>images/button-html5.png" width="80" height="15" alt="Valid HTML5" /></a>
             <a href="//jigsaw.w3.org/css-validator/check/referer?profile=css3" title="Valid CSS" <?php echo $target?>><img
                 src="<?php echo tpl_basedir(); ?>images/button-css.png" width="80" height="15" alt="Valid CSS" /></a>
-            <a href="https://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
-                src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15"
-                alt="Driven by DokuWiki" /></a>
         </div><!-- /.buttons -->
     </div><!-- /.pad -->
 </div><!-- /#dokuwiki__footer -->
