9abc1f1298890331a8349ebcb77aef9d1aa7a9de
Simon DELAGE <sdelage@gmail.com>
2020-05-06 23:22:19 +0200

    Dropdown topbar links when required

--- tpl_header.php
+++ tpl_header.php
@@ -35,16 +35,16 @@ if (!defined('DOKU_INC')) die();
                     </div>
                     <?php
                         if (strpos(tpl_getConf('topbar'), 'links') !== false) {
-                            print '<div id="colormag__topbar-links" class="flex row" title="'.$colormag['topbar-links'].'">';
+                            print '<div id="colormag__topbar-links" class="flex row" title="'.$colormag['topbar-links'].'"><ul><li class="menu-item-has-children">';
                                 if ($colormag['topbar-links']) {
-                                        tpl_link(wl($colormag['topbar-links']), colormag_glyph($colormag['glyphs']['topbar-page'], true).'<span'.((($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? '' : ' class="a11y"').'>'.tpl_getLang("links").'</span>',' title="'.$colormag['topbar-links'].'"');
-                                        tpl_include_page($colormag['topbar-links'], true, true, true); /* includes the topbar links wiki page */
+                                    tpl_link(wl($colormag['topbar-links']), colormag_glyph($colormag['glyphs']['topbar-page'], true).'<span'.((($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? '' : ' class="a11y"').'>'.tpl_getLang("links").'</span>',' title="'.$colormag['topbar-links'].'"');
+                                    tpl_include_page($colormag['topbar-links'], true, true, true); /* includes the topbar links wiki page */
                                 } elseif (auth_quickaclcheck(getns($ID).':'.tpl_getConf('topbarlinkspage')) >= 4) {
-                                        tpl_link(wl(getns($ID).':'.tpl_getConf('topbarlinkspage')).'&do=edit', colormag_glyph($colormag['glyphs']['topbar-page-add'], true).'<span'.((($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? '' : ' class="a11y"').'>'.tpl_getLang('addtopbarlinks').'</span>',' title="'.tpl_getLang('addtopbarlinks').'"');
+                                    tpl_link(wl(getns($ID).':'.tpl_getConf('topbarlinkspage')).'&do=edit', colormag_glyph($colormag['glyphs']['topbar-page-add'], true).'<span'.((($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? '' : ' class="a11y"').'>'.tpl_getLang('addtopbarlinks').'</span>',' title="'.tpl_getLang('addtopbarlinks').'"');
                                 //} else {
                                     //print colormag_glyph($colormag['glyphs']['topbar-page-denied'], true)." permission do add links is denied.";
                                 }
-                            print '</div>';
+                            print '</li></ul></div>';
                         }
                     ?>
                 </div>
