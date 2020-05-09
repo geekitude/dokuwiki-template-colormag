c129a6b3e8bf29c9d7d139840e44a3cf90010328
Simon DELAGE <sdelage@gmail.com>
2020-05-09 19:11:35 +0200

    Untranslated home dropdown
    Fix #3

--- tpl_header.php
+++ tpl_header.php
@@ -174,7 +174,16 @@ if (!defined('DOKU_INC')) die();
                     <div id="colormag__site-navigation-primary" class="flex row start">
                         <ul id="colormag__contools" class="menunav-menu menu-primary-container-left-section">
                             <!-- HOME -->
-                            <li class="menu-item nav home"><a href="<?php print wl() ?>" title="Home"><?php if ((isset($colormag['translation']['ishome'])) and ($colormag['translation']['ishome'] == "translated")) { colormag_glyph($colormag['glyphs']['translated']); } else { colormag_glyph($colormag['glyphs']['home']); } ?><span<?php print (($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? "" : " class='a11y'" ?>>Home</span></a></li>
+                            <?php if($colormag['translation']['ishome'] == "translated"): ?>
+                                <li class="menu-item nav home menu-item-has-children">
+                                    <a href="<?php print wl() ?>" title="Home"><?php colormag_glyph($colormag['glyphs']['translated']); ?><span<?php print (($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? "" : " class='a11y'" ?>>Home</span></a>
+                                    <ul class="sub-menu">
+                                        <li class="menu-item nav home"><a href="<?php print wl($colormag['translation']['untranslatedhome']) ?>" title="Home"><?php colormag_glyph($colormag['glyphs']['home']); ?><span<?php print (($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? "" : " class='a11y'" ?>>Home</span></a></li>
+                                    </ul>
+                                </li>
+                            <?php else: ?>
+                                <li class="menu-item nav home"><a href="<?php print wl() ?>" title="Home"><?php colormag_glyph($colormag['glyphs']['home']); ?><span<?php print (($_GET['debug'] == 1) or ($_GET['debug'] == 'a11y')) ? "" : " class='a11y'" ?>>Home</span></a></li>
+                            <?php endif; ?>
                             <!-- NSINDEX DROPDOWN -->
                             <li id="colormag__nsindex-dropdown" class="menu-item nav nsindex menu-item-has-children sub-toggle"><a href="<?php print wl($ID) ?>&do=index" title="<?php print tpl_getLang('nscontent') ?>"><?php colormag_glyph($colormag['glyphs']['map']) ?></a></li>
                         </ul><!-- /#colormag__contools -->
