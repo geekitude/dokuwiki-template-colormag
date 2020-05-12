60ee636988ecd673035b1ca38eb1e4a06e8055ad
Simon DELAGE <sdelage@gmail.com>
2020-05-12 06:30:33 +0200

    Reintroduced pills-look breadcrumbs labels

--- main.php
+++ main.php
@@ -62,9 +62,9 @@ $showSidebar = $hasSidebar && ($ACT=='show');
                     <!-- PAGEID -->
                     <div class="primary flex column start">
                         <?php colormag_include("pageidheader"); ?>
-                        <div class="breadcrumbs flex row start">
+                        <div class="breadcrumbs">
                             <?php if(($conf['youarehere']) and ($conf['breadcrumbs'])): ?>
-                                <span class="classic" title="<?php print rtrim($lang['youarehere'], ':') ?>"><?php print $lang['youarehere'] ?></span>
+                                <span class="label" title="<?php print rtrim($lang['youarehere'], ':') ?>"><?php print $lang['youarehere'] ?></span>
                                 <ul class="youarehere"><?php colormag_youarehere() ?></ul>
                             <?php else: ?>
                                 <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
@@ -79,15 +79,15 @@ $showSidebar = $hasSidebar && ($ACT=='show');
                     <?php if(($conf['breadcrumbs'] || $conf['youarehere']) or (($colormag['translation']['helper']) and !($conf['youarehere']) and !($conf['breadcrumbs']))): ?>
                         <div class="secondary">
                             <?php colormag_include("breadcrumbsheader"); ?>
-                            <div class="breadcrumbs flex row-reverse start">
+                            <div class="breadcrumbs">
                                 <?php if(($conf['youarehere']) and !($conf['breadcrumbs'])): ?>
-                                    <span class="classic" title="<?php print rtrim($lang['youarehere'], ':') ?>"><?php print $lang['youarehere'] ?></span>
+                                    <span class="label" title="<?php print rtrim($lang['youarehere'], ':') ?>"><?php print $lang['youarehere'] ?></span>
                                     <ul class="youarehere"><?php colormag_youarehere() ?></ul>
                                 <?php endif ?>
                             </div>
-                            <div class="breadcrumbs flex row-reverse start">
+                            <div class="breadcrumbs">
                                 <?php if($conf['breadcrumbs']): ?>
-                                    <span class="classic" title="<?php print rtrim($lang['breadcrumb'], ':') ?>"><?php print $lang['breadcrumb'] ?></span>
+                                    <span class="label" title="<?php print rtrim($lang['breadcrumb'], ':') ?>"><?php print $lang['breadcrumb'] ?></span>
                                     <ul class="trace"><?php colormag_trace() ?></ul>
                                 <?php endif ?>
                                 <?php if(($colormag['translation']['helper']) and !($conf['youarehere']) and !($conf['breadcrumbs'])) print $colormag['translation']['helper']->showTranslations(false); ?>
